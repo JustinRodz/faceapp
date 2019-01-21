996628762e5e01f02fba8a358e1059270ef32481
JustinRodz <46830364+JustinRodz@users.noreply.github.com>
2019-01-21 14:35:06 -0600

    Updates

---
+++ precache-manifest.d41519a9c0e2e291d16b30a8c769be7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bc0188de68c7da46992",
+    "url": "/faceapp/static/js/runtime~main.7bc0188d.js"
+  },
+  {
+    "revision": "b09c93d9db6ced42977d",
+    "url": "/faceapp/static/js/main.b09c93d9.chunk.js"
+  },
+  {
+    "revision": "4d528599400407c00358",
+    "url": "/faceapp/static/js/1.4d528599.chunk.js"
+  },
+  {
+    "revision": "b09c93d9db6ced42977d",
+    "url": "/faceapp/static/css/main.02edf669.chunk.css"
+  },
+  {
+    "revision": "4d528599400407c00358",
+    "url": "/faceapp/static/css/1.642f27b9.chunk.css"
+  },
+  {
+    "revision": "6916b758333bbe781fde8a8f9b33cda4",
+    "url": "/faceapp/index.html"
+  }
+];
